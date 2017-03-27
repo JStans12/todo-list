d7625a96dad0e9d60a5ff997819e76e7606a955f
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-03-27 14:41:16 -0600

    todo-list

---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './List.css';
+
+class List extends Component {
+  render() {
+    return (
+      <ul>
+        <li>things</li>
+        <li>stuff</li>
+      </ul>
+    );
+  }
+}
+
+export default List;
